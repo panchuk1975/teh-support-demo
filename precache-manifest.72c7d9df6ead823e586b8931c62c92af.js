057d7e8c086c3d3c682d35298d5b0a4edfd7102a
panchuk1975 <panchuk1975vadik@gmail.com>
2020-02-04 16:08:59 +0200

    Updates

---
+++ precache-manifest.72c7d9df6ead823e586b8931c62c92af.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9709fefd4da569f3c6e3a3b1a828c4a4",
+    "url": "/teh-support-demo/index.html"
+  },
+  {
+    "revision": "f26d421d9a6190315673",
+    "url": "/teh-support-demo/static/css/3.8d178070.chunk.css"
+  },
+  {
+    "revision": "e72a7f12b6aeae3b15c6",
+    "url": "/teh-support-demo/static/css/4.9b894d67.chunk.css"
+  },
+  {
+    "revision": "3af9f2e3821f2eb9b10e",
+    "url": "/teh-support-demo/static/css/main.c34557cd.chunk.css"
+  },
+  {
+    "revision": "f3e8b4363b6b0be130a9",
+    "url": "/teh-support-demo/static/js/2.206aca88.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/teh-support-demo/static/js/2.206aca88.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f26d421d9a6190315673",
+    "url": "/teh-support-demo/static/js/3.056a2774.chunk.js"
+  },
+  {
+    "revision": "e72a7f12b6aeae3b15c6",
+    "url": "/teh-support-demo/static/js/4.d75f0bb1.chunk.js"
+  },
+  {
+    "revision": "3af9f2e3821f2eb9b10e",
+    "url": "/teh-support-demo/static/js/main.9bc5cbf6.chunk.js"
+  },
+  {
+    "revision": "599879dd740a37d1cf82",
+    "url": "/teh-support-demo/static/js/runtime-main.9b7eeb3e.js"
+  },
+  {
+    "revision": "36a00c6dd37d124fa1fee3c6f69d16e1",
+    "url": "/teh-support-demo/static/media/Red_Sea.36a00c6d.jpg"
+  },
+  {
+    "revision": "7ea39d808e809e992023878ab5880f75",
+    "url": "/teh-support-demo/static/media/preload.7ea39d80.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/teh-support-demo/precache-manifest.72c7d9df6ead823e586b8931c62c92af.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teh-support-demo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
