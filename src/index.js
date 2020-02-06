61df7eaaf1c4c58e78839c217eee4995934bee26
panchuk1975 <panchuk1975vadik@gmail.com>
2020-02-06 15:18:54 +0200

    added index.html file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import store from './Redux/reduxStore';
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 //import StoreContext from "./StoreContext";
 //import {Provider} from "./StoreContext";
 import {Provider} from "react-redux";
@@ -14,8 +14,9 @@ import {Provider} from "react-redux";
 
 let rerenderEntireTree = (props) => {
   ReactDOM.render( // - props => getState()
-    <BrowserRouter basename = {process.env.PUBLIC_RL}>
-    {/*StoreContext.*/}<Provider /*value*/store = {store}>
+  <HashRouter>
+    {/*<BrowserRouter basename = {process.env.PUBLIC_RL}>
+    StoreContext.*/}<Provider /*value*/store = {store}>
       {/* <App state={props} addPost={store.addPost.bind(store)} 
       updateNewPostText = {store.updateNewPostText.bind(store)}
       addMessage={store.addMessage.bind(store)} //- store method !!!
@@ -24,7 +25,8 @@ let rerenderEntireTree = (props) => {
        state = {props}*/}
        <App  />
        {/*StoreContext.*/}</Provider>
-    </BrowserRouter>,//- if not call now we must to bind context
+    {/*</BrowserRouter>*/} 
+    </HashRouter>,//- if not call now we must to bind context
     document.getElementById("root")// to store object
   );
 };
